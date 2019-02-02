13d871ea4ce8e8cf1aa1298b3f17df61cd5f4152
dilorom77 <46678142+dilorom77@users.noreply.github.com>
2019-02-01 23:01:45 -0500

    Update LoginTests

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -28,6 +28,7 @@ public class LoginTests {
         driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/login.aspx");
         driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
         driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test" + Keys.ENTER);
+        String title = driver.getTitle();
         Assert.assertEquals(driver.getTitle(), "Web Orders");
     }
 
